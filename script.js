d24286d16cc27d7150bd0ff625ccf7ba899bb0fe
elev180284 <89663773+elev180284@users.noreply.github.com>
2021-08-31 13:13:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var colors = ["red", "blue", "green"];
+var colors = ["red", "blue", "green", "pink", "purple", "yellow"];
     var colorIndex = 0;
     function changeColor() {
         var col = document.getElementById("body");
@@ -11,6 +11,11 @@ var colors = ["red", "blue", "green"];
     document.addEventListener("keyup", function(event) {
         if (event.keyCode === 70) {
             alert('You Payed Respects!');
+            alert('You Payed Respects 1!');
+            alert('You Payed Respects 2!');
+            alert('You Payed Respects 3!');
+            alert('You Payed Respects 4!');
+            alert('You Payed Respects 5!');
 
         }
     });
